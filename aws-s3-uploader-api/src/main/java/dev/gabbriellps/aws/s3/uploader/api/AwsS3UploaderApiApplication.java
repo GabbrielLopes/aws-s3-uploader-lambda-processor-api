1a5f1f962756495b1724be631e42084ab59c17ef
GabbrielLps <jhosegabriel95@gmail.com>
2024-05-16 20:48:14 -0300

    feat: cria estrutura api
    feat: cria estrutura api

---
+++ aws-s3-uploader-api/src/main/java/dev/gabbriellps/aws/s3/uploader/api/AwsS3UploaderApiApplication.java
@@ -0,0 +1,13 @@
+package dev.gabbriellps.aws.s3.uploader.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsS3UploaderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsS3UploaderApiApplication.class, args);
+	}
+
+}
---
+++ aws-s3-uploader-api/src/test/java/dev/gabbriellps/aws/s3/uploader/api/AwsS3UploaderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gabbriellps.aws.s3.uploader.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsS3UploaderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
